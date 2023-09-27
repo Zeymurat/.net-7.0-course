91e930e16e44c3a7367d081c77d3a3ce88a98b0c
Zeymurat <cumhurmurat859@gmail.com>
2023-09-28 00:28:58 +0300

    Add project files.

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basics.Controllers
+{
+    // course
+    public class CourseController : Controller
+    {
+        // course/index
+        public string Index()
+        {
+            return "course/index";
+        }
+        // course/list
+        public string List()
+        {
+            return "course/list";
+        }
+        //public IActionResult Index()
+        //{
+        //    return View(); 
+        //}
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Basics.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Basics.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "home/index";
+        }
+
+        public string Contact()
+        {
+            return "home/contact";
+        }
+    }
+}
